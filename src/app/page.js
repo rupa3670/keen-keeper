57a1bb1a7ac5dbe4e7dd6554f34fd0744a490bfe
Tanjila Rupa <srrupaliakter@gmail.com>
2026-04-24 20:25:17 +0600

    design home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,79 @@
+"use client";
 import { Plus } from 'lucide-react';
-export const metadata = {
-  title: "Home | keen keeper",
+import { useEffect, useState } from 'react';
 
-};
+
 export default function Home() {
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    fetch('/friends.json')
+      .then((res) => res.json())
+      .then((data) => setFriends(data))
+  }, []);
+  const totalFriends = friends.length;
+  const onTrack = friends.filter(f => f.status === "on-track").length;
+  const needAttention = friends.filter(f => f.status === "overdue" || f.status === "almost due").length;
   return (
-  <>
-    <div className="flex flex-col justify-center items-center text-center mt-15 mb-8">
-      <h2 className="text-5xl font-bold">Friends to keep close in your life </h2>
-      <p className="text-gray-500 mt-4 mb-6">Your personal shelf of meaningful connections. Browse, tend, and nurture the <br/>
-        relationships that matter most.</p>
-         <button className="btn bg-[#244D3F] text-white">  <Plus size={18}/>Add Friend</button>
+    <>
+    <div className='min-h-screen bg-gray-50 pb-10'>
+        <div className="flex flex-col justify-center items-center text-center pt-10  md:pt-16 px-4 mb-7">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold">Friends to keep close in your life </h2>
+        <p className="text-gray-500 mt-4 mb-6 ">Your personal shelf of meaningful connections. Browse, tend, and nurture the <br />
+          relationships that matter most.</p>
+        <button className="btn bg-[#244D3F] text-white">  <Plus size={18} />Add Friend</button>
+      </div>
+      <div className='max-w-6xl mx-auto px-4 md:px-10   grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-4   mb-7 text-xl font-bold text-[#244D3F] '>
+        <div className='flex flex-col justify-center items-center text-center  bg-white shadow-sm rounded rounded-[8px] py-4 md:py-8'>
+          <h2>{totalFriends}</h2>
+          <p className='text-sm font-semibold text-gray-500 '>Total Friends</p>
+        </div>
+        <div className='flex flex-col justify-center items-center text-center  bg-white shadow-sm rounded rounded-[8px] py-4 md:py-8'>
+          <h2>{onTrack}</h2>
+          <p className='text-sm font-semibold text-gray-500'>On Track</p>
+        </div>
+        <div className='flex flex-col justify-center items-center text-center  bg-white shadow-sm rounded rounded-[8px] py-4 md:py-8'>
+          <h2>{needAttention}</h2>
+          <p className='text-sm font-semibold text-gray-500'>Need Attention</p>
+        </div>
+        <div className='flex flex-col justify-center items-center text-center  bg-white shadow-sm rounded rounded-[8px] py-4 md:py-8'>
+          <h2>12</h2>
+          <p className='text-sm font-semibold text-gray-500'>Interactions This Month</p>
+        </div>
+      </div>
+      <div className='max-w-7xl mx-auto px-4 md:px-10 lg:px-20 border-t border-gray-300  pt-10 '>
+        <h3 className='text-2xl fond-bold mb-6'>Your Friends</h3>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
+          {friends.map((friend) =>
+            <div key={friend.id} className="bg-white shadow-sm border border-gray-100 rounded-[20px] p-6 flex flex-col items-center">
+             
+                <div className='mb-4'> <img
+                  src={friend.picture}
+                  alt={friend.name}
+                  className="rounded-full w-20 h-20 object-cover " /></div>
+             
+              <div className="flex flex-col items-center text-center w-full">
+                <h2 className="text-lg font-bold text-gray-800 ">{friend.name}</h2>
+                <p className='text-gray-500 text-sm mb-3'>{friend.days_since_contact}d ago</p>
+                <div className='flex flex-wrap justify-center gap-2 mb-3'>
+                  {friend.tags.map(tag => (
+                    <span key={tag} className='px-3 py-0.5 rounded-full text-[10px] font-bold uppercase bg-green-100 text-green-700'>{tag}</span>
+                  ))}
+                </div>
+                <div className={`w-full py-2 rounded-full text-[11px] font-bold text-center text-white ${friend.status==='overdue' ? 'bg-red-400' : friend.status==='almost due' ?'bg-orange-400':'bg-green-400'}`}>
+                  {friend.status.toUpperCase()}
+
+                </div>
+              </div>
+            </div>
+          )};
+        </div>
+      </div>
     </div>
-   
-  </>
+
+
+
+
+
+    </>
   );
 }
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import Link from 'next/link';
-import { House,Clock4,ChartLine,  } from 'lucide-react';
+import { House, Clock4, ChartLine, Menu, } from 'lucide-react';
 
 const Navbar = () => {
-    return (
-        <div className='bg-white shadow-sm border-b border-gray-100'>
-            <div className="navbar mx-auto px-4 md:px-10 ">
-  <div className="flex-1">
-    <h2 className=" text-2xl font-bold">Keen<span className='text-[#244D3F]'>Keeper</span></h2>
-  </div>
-  <div className="flex-none ">
-    <ul className="menu menu-horizontal   text-gray-500  font-medium">
-      <li><Link href="" className='bg-[#244D3F] text-white hover:bg-[#68bea0]'><House size={18}/>Home</Link></li>
-      <li><Link href="/timeline"><Clock4 size={18}/>Timeline</Link></li>
+  const navLinks = (
+    <>
+      <li><Link href="" className='bg-[#244D3F] text-white hover:bg-[#68bea0]'><House size={18} />Home</Link></li>
+      <li><Link href="/timeline"><Clock4 size={18} />Timeline</Link></li>
       <li><Link href="/stats"><ChartLine size={18} />Stats</Link></li>
-    </ul>
-  </div>
-</div>
+    </>
+  )
+  return (
+    <div className='bg-white shadow-sm border-b border-gray-100'>
+      <div className="navbar max-w-7xl mx-auto px-4 md:px-10 ">
+        <div className='navbar-start'>
+          <div className='dropdown'>
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden p-1">
+              <Menu size={24} />
+            </div>
+            <ul tabIndex={0} className='menu menu-sm dropdown-content bg-base-100 rounded-box  z-1 mt-3 w-52 p-2 shadow'>{navLinks}</ul>
+          </div>
+          <h2 className=" text-xl md:text-2xl ml-2 lg:ml-0 font-bold">Keen<span className='text-[#244D3F]'>Keeper</span></h2>
+
         </div>
-    );
+
+
+        <div className=" navbar-end hidden lg:flex ">
+          <ul className="menu menu-horizontal   text-gray-500  font-medium">
+            {navLinks}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
